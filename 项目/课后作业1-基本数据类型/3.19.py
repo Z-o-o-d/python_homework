340316c8d5bc7cd0fe32247e225f0814d73b65f1
JINZE-CN <874074406@qq.com>
2023-10-08 22:22:01 +0800

    1

---
+++ 项目/课后作业1-基本数据类型/3.19.py
@@ -0,0 +1,14 @@
+s="Python String"
+print(s.upper())
+
+s="Python String"
+print(s.lower())
+
+s="Python String"
+print(s.find('i'))
+
+s="Python String"
+print(s.replace('ing','gni'))
+
+s="Python String"
+print(s.split(' '))
---
+++ 项目/课后作业1-基本数据类型/3.8.py
@@ -0,0 +1,8 @@
+import math
+print(math.sin(2*math.pi))
+print(math.floor(-2.5))
+print(math.ceil(3.5+math.floor(-2.5)))
+print(round(math.fabs(-2.5)))
+print(math.sqrt(math.pow(2,4)))
+print(math.log(math.e))
+      
